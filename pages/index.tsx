547c69b7f46aafebb9ad827d8116c795572bee30
JohnReyAsong <ajohnrey15@gmail.com>
2021-08-06 18:58:23 +0800

    Commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,49 @@
+import { Box, Button, Container, Divider, Flex, Heading, Spacer, Wrap } from '@chakra-ui/react';
+import ProductCard from '@components/ProductCard';
+import NextLink from 'next/link';
 import { FC } from 'react';
+import { BsPlus } from 'react-icons/bs';
 
-const Home: FC = () => {
-    return (
-        <div >
-            Index
-        </div>
-    )
-};
+
+
+
+const Home: FC = () => (
+    <Container maxW="container.xl" alignItems="center" background="gray.50" paddingTop="97px" paddingBottom="97px">
+        <Flex display="flex">
+            <Box>
+                <Heading size="lg">Products</Heading>
+            </Box>
+            <Spacer />
+            <Box>
+                <NextLink href="/product/add">
+                    <Button leftIcon={<BsPlus size="20" />} colorScheme="purple" fontWeight="bold">
+                        Add Product
+                    </Button>
+                </NextLink>
+            </Box>
+
+        </Flex>
+
+        <Divider mb={8} mt={3} />
+
+        <Wrap spacing="29px" justify="center" >
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+        </Wrap>
+
+        <Divider mt={8} />
+    </Container>
+
+);
 
 export default Home;
